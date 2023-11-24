00305b15427c6d1587ebe327b2e3189c38df79e3
Matvei Stremous <matvei.stremous@gmail.com>
2023-11-24 23:54:28 +0300

    add cors configuration

--- common-service/src/main/java/com/example/commonservice/config/SecurityConfig.java
+++ common-service/src/main/java/com/example/commonservice/config/SecurityConfig.java
@@ -23,7 +23,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/auth/login", "/auth/signup").permitAll()
