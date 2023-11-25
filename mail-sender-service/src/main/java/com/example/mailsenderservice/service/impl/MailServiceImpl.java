feb5ce179a2c28d20f26ce14eee423d70fd98815
Matvei Stremous <matvei.stremous@gmail.com>
2023-11-25 10:14:02 +0300

    add mail sender application name

--- mail-sender-service/src/main/java/com/example/mailsenderservice/service/impl/MailServiceImpl.java
+++ mail-sender-service/src/main/java/com/example/mailsenderservice/service/impl/MailServiceImpl.java
@@ -2,24 +2,34 @@ package com.example.mailsenderservice.service.impl;
 
 import com.example.mailsenderservice.dto.MailKafkaDto;
 import com.example.mailsenderservice.service.MailService;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MailServiceImpl implements MailService {
     private final JavaMailSender mailSender;
+    @Value("${mail.username}")
+    private String username;
+    @Value("${mail.application.name}")
+    private String applicationName;
 
     @Override
+    @SneakyThrows
     public void send(MailKafkaDto mailKafkaDto) {
         if (mailKafkaDto.getConsumerEmail() != null && !mailKafkaDto.getConsumerEmail().isEmpty()) {
-            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
-            simpleMailMessage.setTo(mailKafkaDto.getConsumerEmail());
-            simpleMailMessage.setSubject(mailKafkaDto.getTopicName());
-            simpleMailMessage.setText(mailKafkaDto.getMessage());
-            mailSender.send(simpleMailMessage);
+            MimeMessage mailMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mailMessage, true, "UTF-8");
+            helper.setFrom(username, applicationName);
+            helper.setTo(mailKafkaDto.getConsumerEmail());
+            helper.setSubject(mailKafkaDto.getTopicName());
+            helper.setText(mailKafkaDto.getMessage());
+            mailSender.send(mailMessage);
         }
     }
 }
